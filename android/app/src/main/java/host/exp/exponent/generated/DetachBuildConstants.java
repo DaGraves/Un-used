a8c8b7dcaaebb34c6a80c6a659d848778687c793
Sérgio Reis <sergio.reis@hydradev.io>
2020-01-09 12:45:39 +0000

    expo eject

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expa70c4800c234435bbe88061a02478dbc://192.168.0.183:19000";
+
+}
